c91b79706a718e7138f60b85ad0a5573b5f07712
S0193SathrSarathin <sathrsarathin653@gmail.com>
2025-05-24 17:35:59 +0700

    Add files via upload

---
+++ เว็บแนะนำตัว/vanta-init.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.CELLS({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200,
+    minWidth: 200,
+    scale: 1.0,
+    color1: 0x094a4a,
+    color2: 0xe8a0c1,
+    backgroundAlpha: 0.7
+  });
+});
